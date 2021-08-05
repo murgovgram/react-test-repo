ec115ce6b026db291d257c9dc3a52d14fb988e4b
Filip Murgovski <murgovski@gmail.com>
2021-08-05 22:01:33 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import Counter from './components/test';
+
+function App() {
+  return (
+    <Example>
+    </Example>
+  );
+}
+
+export default App;
